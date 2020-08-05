7adea319ed57984a8f65294b05ffa706412d8207
DomagojMuza <domim1998@gmail.com>
2020-08-05 20:33:48 +0200

    first upload

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const name = "Hi, my name is Domagoj"
-const aboutMe = "Software engineere and programmer"
-
 window.onload = function(){
     ScrollOut();
-    document.getElementById("name").innerHTML = name;
-    document.getElementById("aboutMe").innerHTML = aboutMe;
 }
